aa47ba6edde723992c93291a4dfa355d678f2dcb
Thiago Fernando Procorio <thiago.procorio@loglabdigital.com.br>
2020-03-06 12:01:32 -0400

    Add Lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import Speech from 'speak-tts';
+import { ListaModel } from './lista-model.model';
+import { Guid } from "guid-typescript";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Atedimento';
+  listaTabela: ListaModel[] = [];
 
-
+  addLista(texto: string){
+      let p ={
+        id: Guid.create(),
+        nome: texto
+      }
+      console.log(this.listaTabela.length);
+      if(this.listaTabela.length < 4)
+          this.listaTabela.push(p);
+      else
+        alert("Permitido somente 4 registros");
+  }
+  removerItem(index) {
+    this.listaTabela.splice(index, 1);
+  }
   falar(texto: string) {
     if (texto) {
       const speech = new Speech();
---
+++ src/app/lista-model.model.ts
@@ -0,0 +1,6 @@
+import { Guid } from "guid-typescript";
+
+export class ListaModel {
+    public id:Guid;
+    public nome:string;
+}
